5416d54af2d030b56910e0b1d84ac596931a6e1a
Tjebbe <tjebbemarchand@icloud.com>
2019-02-13 12:38:38 +0100

    Refactord my code to object literals and moved it to a new folder called app

---
+++ app/public/js/app.js
@@ -0,0 +1,99 @@
+'use strict';
+
+(function () {
+    // Filters object.
+    const state = {
+        filters: {
+            search: '',
+            sortBy: 'name',
+            filterBy: undefined,
+        },
+        recipes: []
+    };
+
+    // DOM object.
+    const dom = {
+        app: document.querySelector('main'),
+        container: document.querySelector('.container'),
+        searchForm: document.querySelector('#searchForm'),
+        inputRecipeField: document.querySelector('#inputRecipe'),
+        searchRecipeBtn: document.querySelector('#searchRecipe')
+    };
+
+    // API actor
+    const api = {
+        proxy: 'https://cors-anywhere.herokuapp.com/',
+        url: 'https://api.edamam.com/search?q=',
+        entries: {
+            app_id: 'c83b21f1',
+            app_key: 'f1e2173ac672d053a64913c59ad6932b',
+        },
+        createURL: function() {
+            const URLOptions = Object.entries(this.entries)
+                .map(entry => entry.join("="))
+                .join("&");
+            const url = `${this.url}${state.filters.search}&${URLOptions}`;
+            console.log(url);
+            return url;
+        },
+        get: function() {
+            // API request
+            let request = new XMLHttpRequest();
+            request.open('GET', this.createURL(), true);
+
+            request.onload = function () {
+                const data = api.parse(this.response);
+
+                if(request.status >= 200 && request.status < 400) {
+                    api.store(data);
+                    render.recipes();
+                } else {
+                    console.log('error');
+                    const errorMessage = createElement('p');
+                    errorMessage.textContent = 'Something went wrong here';
+                    app.appendChild(errorMessage);
+                }
+            };
+            request.send();
+        },
+        store: function(data) {
+            data.hits.forEach(hit => {
+                state.recipes.push({
+                    title: hit.recipe.label,
+                    image: hit.recipe.image,
+                    ingredients: hit.recipe.ingredientLines,
+                    calories: hit.recipe.calories
+                });
+            });
+        },
+        parse: function(response) {
+            return JSON.parse(response);
+        }
+    };
+
+    // Render actor
+    const render = {
+        recipes: function() {
+            state.recipes.forEach((recipe) => {
+                const imageEl = document.createElement('img');
+                imageEl.src = recipe.image;
+    
+                const titleEl = document.createElement('h2');
+                titleEl.textContent = recipe.title;
+    
+                dom.container.appendChild(imageEl);
+                imageEl.insertAdjacentElement('afterend', titleEl);
+            });
+        },
+        clear: function() {
+            container.innerHTML = ''; // Clear the search results.
+        }
+    };
+
+    dom.searchRecipeBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        state.filters.search = dom.inputRecipeField.value;
+        api.get();
+        dom.inputRecipeField.value = '';
+    });
+})();
